f1a1fc0d4e45597d9f44f5e59910c2b5ed19221a
calebjc3 <43564607+calebjc3@users.noreply.github.com>
2022-06-30 22:58:34 -0700

    changes to include cart updates

--- pharma/models.py
+++ pharma/models.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
 class med(models.Model):
     medName = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField()
+
 
     def publish(self):
         self.save()
